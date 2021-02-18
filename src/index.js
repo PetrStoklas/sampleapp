38037e6339fcaec06dca991036184a0acb216e9f
Petr Stoklas <petrstoklas@gmail.com>
2021-02-18 11:16:25 +0100

    Get valid access token added.

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { IKUIInit } from "indykite-ui-sdk";
+import { IKUIInit, IKUIUserAPI } from "indykite-ui-sdk";
 
 IKUIInit({
   baseUri: process.env.REACT_APP_BASE_URI,
   clientId: process.env.REACT_APP_APPLICATION_ID,
   tenantId: process.env.REACT_APP_TENANT_ID,
 });
+
+// Returns valid token if possible. You can pass refresh_token as parameter.
+IKUIUserAPI.getValidAccessToken().then(console.log).catch(console.log)
 
 ReactDOM.render(
   <React.StrictMode>
