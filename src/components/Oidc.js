8f42bd8bf17e43a4be7672122a8b21c5573164d5
Petr Stoklas <petrstoklas@gmail.com>
2021-02-23 22:18:00 +0100

    Oidc callback component.

--- src/components/Oidc.js
+++ src/components/Oidc.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { IKUIOidc } from "indykite-ui-sdk";
 
+// This is used when Indykite is the provider
 const Oidc = () => {
     console.log('I was here')
     React.useEffect(() => {
@@ -13,7 +14,17 @@ const Oidc = () => {
         sparkOidc();
     });
 
-    return <h1>oidc</h1>;
+    return <h3>oidc</h3>;
 };
 
+
+// This is where FB, Google and other providers can redirect you
+const Callback = () => {
+    React.useEffect(() => {
+        IKUIOidc.oidcCallback().then(console.log).catch(console.log)
+    })
+    return <h3>general callback</h3>
+}
+
 export default Oidc;
+export { Callback };
